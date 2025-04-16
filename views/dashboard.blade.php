5bac1c848228c295b032ce2fdd58902519237fa3
KurAnt1313 <148110487+RizkyKurniaAntasari@users.noreply.github.com>
2025-04-16 11:41:07 +0700

    Update dashboard.blade.php
    ganti font dari sans jadi poppins

--- views/dashboard.blade.php
+++ views/dashboard.blade.php
@@ -1,57 +1,3 @@
-{{-- <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel 11 Custom Dashboard - ItSolutionStuff.com</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-  </head>
-<body>
-    
-<main>
-  <div class="container py-4">
-    <header class="pb-3 mb-4 border-bottom">
-        <div class="row">
-            <div class="col-md-11">
-                <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-                    <img src="https://www.itsolutionstuff.com/assets/images/logo-it-2.png" alt="BootstrapBrain Logo" width="300">
-                </a>          
-            </div>
-            <div class="col-md-1">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </div>
-      
-    </header>
-
-    <div class="p-5 mb-4 bg-light rounded-3">
-      <div class="container-fluid py-5">
-
-        @session('success')
-            <div class="alert alert-success" role="alert"> 
-              {{ $value }}
-            </div>
-        @endsession
-
-        <h1 class="display-5 fw-bold">Hi, {{ auth()->user()->name }}</h1>
-        <p class="col-md-8 fs-4">Welcome to dashboard.<br/>Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button class="btn btn-primary btn-lg" type="button">Dashboard</button>
-      </div>
-    </div>
-
-  </div>
-</main>
-
-</body>
-</html> --}}
 <!DOCTYPE html>
 <html lang="id">
 
@@ -60,9 +6,16 @@
     <title>SIMAPAN - Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+<style>
+    body {
+        font-family: 'Poppins', sans-serif;
+    }
+</style>
 
-<body class="bg-green-100 font-sans">
-    
+
+<body class="bg-green-100 font-poppins">
+
     <x-navbar />
 
     <!-- Hero Section -->
@@ -73,7 +26,8 @@
         </div>
 
         <!-- Tengah -->
-        <div class="md:col-span-1 overflow-hidden relative transition-transform hover:scale-105 ease-in-out duration-300">
+        <div
+            class="md:col-span-1 overflow-hidden relative transition-transform hover:scale-105 ease-in-out duration-300">
             <img src="\img\tengah.jpg" alt="" class="rounded-md w-full h-64 object-cover">
             <div class="absolute bottom-0 bg-black bg-opacity-60 text-white p-4 w-full">
                 <h2 class="text-lg font-semibold">Tren Kopi Spesialti: Meningkatnya Minat Konsumen terhadap Kopi
@@ -87,30 +41,6 @@
             <img src="\img\cabai.jpeg" alt="cabai.jpeg" class="rounded-md w-full h-full object-cover">
         </div>
     </section>
-
-    <!-- Hero Section [ maybe ] -->
-{{-- <section class="flex overflow-x-auto space-x-4 p-6 bg-white">
-    <!-- Gambar kiri -->
-    <div class="min-w-[300px] overflow-hidden transition-transform hover:scale-105 ease-in-out duration-300">
-        <img src="\img\left.jpg" alt="Tanaman" class="rounded-md w-full h-full object-cover">
-    </div>
-
-    <!-- Tengah -->
-    <div class="min-w-[300px] overflow-hidden relative transition-transform hover:scale-105 ease-in-out duration-300">
-        <img src="\img\tengah.jpg" alt="" class="rounded-md w-full h-64 object-cover">
-        <div class="absolute bottom-0 bg-black bg-opacity-60 text-white p-4 w-full">
-            <h2 class="text-lg font-semibold">Tren Kopi Spesialti: Meningkatnya Minat Konsumen terhadap Kopi
-                Berkualitas Tinggi</h2>
-            <p class="text-sm mt-1">23 Maret 2025</p>
-        </div>
-    </div>
-
-    <!-- Gambar kanan -->
-    <div class="min-w-[300px] overflow-hidden transition-transform hover:scale-105 ease-in-out duration-300">
-        <img src="\img\cabai.jpeg" alt="cabai.jpeg" class="rounded-md w-full h-full object-cover">
-    </div>
-</section> --}}
-
 
     <!-- Artikel + Statistik -->
     <section class="grid grid-cols-1 md:grid-cols-2 gap-6 px-6 py-6 bg-green-200">
@@ -251,7 +181,5 @@
         </div>
     </footer>
 
-
 </body>
-
 </html>
